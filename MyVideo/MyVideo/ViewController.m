f110fd9f4ab28e457c8f29c0e85abc2dc29310ca
mango <690852195@qq.com>
2018-04-11 09:34:53 +0800

    增加了捕捉文件磁盘写入功能

--- MyVideo/MyVideo/ViewController.m
+++ MyVideo/MyVideo/ViewController.m
@@ -12,12 +12,16 @@
 
 /** 捕捉视频
  */
-@interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate,AVCaptureAudioDataOutputSampleBufferDelegate>
+@interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate, // 视频输出代理
+                            AVCaptureAudioDataOutputSampleBufferDelegate,  // 音频输出代理
+                            AVCaptureFileOutputRecordingDelegate           // 文件写入代理(直接将采集到的视频写入文件)
+                            >
 
 @property(nonatomic, strong)AVCaptureSession *session;
 @property(nonatomic, strong)AVCaptureVideoPreviewLayer *previewLayer;
-@property(nonatomic, strong)AVCaptureConnection *audioConnection;
-@property(nonatomic, strong)AVCaptureDeviceInput *videoInput;
+@property(nonatomic, strong)AVCaptureDeviceInput *camaraDevInput;
+@property(nonatomic, strong)AVCaptureVideoDataOutput *videoDataOutput;
+@property(nonatomic, strong)AVCaptureMovieFileOutput *videoFileOutput ;
 @end
 
 @implementation ViewController
@@ -44,8 +48,12 @@
     
     //5、开始采集
     [self.session startRunning];
+    
+    [self saveViewDataOutputToFile];
 }
 - (IBAction)stopRecordBtnClick:(id)sender {
+    // 停止文件写入
+    [self.videoFileOutput  stopRecording];
     
     [self.session stopRunning];
     self.session = nil;
@@ -53,98 +61,41 @@
     self.previewLayer = nil;
     
 }
-- (IBAction)changeCamera:(id)sender {
-    
-    //切换摄像头的主要步骤
-    
-    // 1 获取之前的摄像头
 
-    // 2 获取当前应该显示的摄像头
-    AVCaptureDevice *currentDev = nil;
-    NSArray<AVCaptureDevice *> *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-    for (AVCaptureDevice *dev in devices) {
-        
-        if (dev.position != self.videoInput.device.position) {
-            currentDev = dev;
-            break;
-        }
-    }
-    
-    if (currentDev == nil) {
-        NSLog(@"没有其他的摄像头可以 使用, 切换摄像头失败");
-        return;
-    }
-    
-    // 3 根据当前应该显示的摄像头的Device 创建新的 input
-    NSError *err = nil;
-   AVCaptureDeviceInput *currentInput = [AVCaptureDeviceInput deviceInputWithDevice:currentDev error:&err];
-    if (err) {
-        NSLog(@"切换摄像头时 出错");
-        return;
-    }
-    self.session;
 
-    // 4 在Session 中切换input
-    //session 的配置需要在这个Session 事物中立配置
+/** 将采集到的输出写入文件  */
+-(void)saveViewDataOutputToFile{
+    
+    // 创建写入文件的输出
+    AVCaptureMovieFileOutput *videoFileOutput = [[AVCaptureMovieFileOutput alloc]init];
+    AVCaptureConnection *connection = [videoFileOutput connectionWithMediaType:AVMediaTypeVideo];
+    [connection setAutomaticallyAdjustsVideoMirroring:YES];
+    
     [self.session beginConfiguration];
-    [self.session removeInput:self.videoInput];
-    [self.session addInput:currentInput];
-    self.videoInput = currentInput;
+    if([self.session canAddOutput:videoFileOutput]){
+        [self.session addOutput:videoFileOutput];
+    }
     [self.session commitConfiguration];
     
+    self.videoFileOutput =  videoFileOutput;
     
-    
+    NSString *filePath = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"test.mp4"];
+    NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
+    [videoFileOutput startRecordingToOutputFileURL:fileUrl recordingDelegate:self];
 }
 
 
 #pragma mark- 私有方法
 
--(BOOL)setupAudioCature{
-    
-    // 设置音频的输入（话筒、麦克风）
-   AVCaptureDevice *audoiDev = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
-    if (audoiDev == nil) {
-        NSLog(@"当前的话筒不可用");
-        return NO;
-    }
-    
-    
-    // 创建输入源
-    NSError *err = nil;
-    AVCaptureDeviceInput *audioInput = [AVCaptureDeviceInput deviceInputWithDevice:audoiDev error:&err];
-    if (err) {
-        NSLog(@"设置音频输入源出错");
-        return NO;
-    }
-    
-    // 将音频输入源添加到session
-    [self.session addInput:audioInput];
-    
-    
-    // 给音频设置音频输出源
-    AVCaptureAudioDataOutput *audioOutput = [[AVCaptureAudioDataOutput alloc]init];
-    [audioOutput setSampleBufferDelegate:self queue:dispatch_get_global_queue(0, 0)];
-    [self.session addOutput:audioOutput];
-    self.audioConnection = [audioOutput connectionWithMediaType:AVMediaTypeAudio];
-    return YES;
-    
-    
-}
+
 
 
 -(BOOL)setupVideoCapture{
     //1、创建捕捉会话
-    [self session];
     
-    
-    //2、给捕捉会话添加输入源
-    // 获取前置摄像头和后置摄像头
-    NSArray<AVCaptureDevice *> *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-    if(devices.count == 0){
-        NSLog(@"用户当前的摄像头部能使用");
-        return NO;
-    }
-    
+    //2、创建输入源
+   // NSArray<AVCaptureDevice *> *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    NSArray<AVCaptureDevice *> *devices = [AVCaptureDevice devices];
     // 从前置摄像和后置摄像中选择后置摄像头
     AVCaptureDevice *backGroundDev = nil;
     for (AVCaptureDevice *dev  in devices) {
@@ -158,25 +109,95 @@
         return NO;
     }
     NSError *err = nil;
-    AVCaptureDeviceInput *devInput = [AVCaptureDeviceInput deviceInputWithDevice:backGroundDev error:&err];
-    self.videoInput = devInput;
+    AVCaptureDeviceInput *camaraDevInput = [AVCaptureDeviceInput deviceInputWithDevice:backGroundDev error:&err];
+    
     if(err){
         NSLog(@"设置摄像头捕捉输入时出错");
         return NO;
     }
+    self.camaraDevInput = camaraDevInput;
     
-    [self.session addInput:devInput];
     
-    //3、给捕捉会话设置输出源
+    //3、创建输出源
     AVCaptureVideoDataOutput *videoDataOutput = [[AVCaptureVideoDataOutput alloc]init];
     [videoDataOutput setSampleBufferDelegate:self queue:dispatch_get_global_queue(0, 0)];
-    [self.session addOutput:videoDataOutput];
-
+    self.videoDataOutput = videoDataOutput;
+   
+    //4. 添加输入输出源
+    [self addDevInPut:camaraDevInput dataOutput:videoDataOutput];
+    
     
     return YES;
     
 }
 
+- (IBAction)changeCamera:(id)sender {
+    
+    //切换摄像头的主要步骤
+    
+    // 1 获取之前的摄像头
+    
+    // 2 获取当前应该显示的摄像头
+    AVCaptureDevice *currentCameraDev = nil;
+    NSArray<AVCaptureDevice *> *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *cameraDev in devices) {
+        
+        if (cameraDev.position != self.camaraDevInput.device.position) {
+            currentCameraDev = cameraDev;
+            break;
+        }
+    }
+    
+    if (currentCameraDev == nil) {
+        NSLog(@"没有其他的摄像头可以 使用, 切换摄像头失败");
+        return;
+    }
+    
+    // 3 根据当前应该显示的摄像头的Device 创建新的 input
+    NSError *err = nil;
+    AVCaptureDeviceInput *cameraDevInput = [AVCaptureDeviceInput deviceInputWithDevice:currentCameraDev error:&err];
+    if (err) {
+        NSLog(@"切换摄像头时 出错");
+        return;
+    }
+    
+    
+    // 4 在Session 中切换input
+    [self.session beginConfiguration];
+    
+    [self.session removeInput:self.camaraDevInput];
+    // 添加输入源
+    if ([self.session canAddInput:cameraDevInput]) {
+        [self.session addInput:cameraDevInput];
+        self.camaraDevInput = cameraDevInput;
+    }
+    
+    [self.session commitConfiguration];
+    
+    
+}
+
+
+#pragma mark- 私有方法
+
+/** 添加输入 输入源*/
+-(void)addDevInPut:(AVCaptureInput *)input dataOutput:(AVCaptureOutput *)dataOutPut{
+    [self.session beginConfiguration];
+    // 添加输入源
+    if ([self.session canAddInput:input]) {
+        [self.session addInput:input];
+    }
+    
+    if([self.session canAddOutput:dataOutPut]){
+        [self.session addOutput:dataOutPut];
+    }
+    
+    [self.session commitConfiguration];
+    
+}
+
+
+/** 设置预览图层*/
 -(void)setupPreviewLayer{
     
     AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:self.session];
@@ -186,35 +207,71 @@
 }
 
 
+-(BOOL)setupAudioCature{
+    
+    // 设置音频的输入（话筒、麦克风）
+    AVCaptureDevice *audoiDev = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
+    if (audoiDev == nil) {
+        NSLog(@"当前的话筒不可用");
+        return NO;
+    }
+    
+    
+    // 创建输入源
+    NSError *err = nil;
+    AVCaptureDeviceInput *audioDevInput = [AVCaptureDeviceInput deviceInputWithDevice:audoiDev error:&err];
+    if (err) {
+        NSLog(@"设置音频输入源出错");
+        return NO;
+    }
+    
+    // 创建音频设置音频输出源
+    AVCaptureAudioDataOutput *audioOutput = [[AVCaptureAudioDataOutput alloc]init];
+    [audioOutput setSampleBufferDelegate:self queue:dispatch_get_global_queue(0, 0)];
+    
+    [self addDevInPut:audioDevInput dataOutput:audioOutput];
+    
+    return YES;
+    
+    
+}
+
 
 #pragma mark- 视频代理
 - (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection{
     
-    
-    // 说明这里也可以通过 connection 来区分是音频还是视频的输出
-    // 方式1
-    
-//    if (![audioConnection isEqual:connection]) {
-//        NSLog(@"视频数据输出了");
-//    }
-//    else{
-//          NSLog(@"音频输出  音频输出");
-//
-//    }
-    
-    
-    // 方式2
-    // 视频输出
-    if ([output isKindOfClass:[AVCaptureVideoDataOutput  class]]) {
+
+    if ([[self.videoDataOutput connectionWithMediaType:AVMediaTypeVideo] isEqual:connection]) {
          NSLog(@"视频数据输出了");
     }
-    // 音频输出
-    else if([output isKindOfClass:[AVCaptureAudioDataOutput  class]]){
-         NSLog(@"音频输出  音频输出");
+    else{
         
+          NSLog(@"音频输出  音频输出");
     }
-   
+
     
+}
+
+#pragma mark- AVCaptureFileOutputRecordingDelegate
+- (void)captureOutput:(AVCaptureFileOutput *)output didStartRecordingToOutputFileAtURL:(NSURL *)fileURL fromConnections:(NSArray<AVCaptureConnection *> *)connections{
+    
+    
+    NSLog(@"开始写入文件");
+}
+
+- (void)captureOutput:(AVCaptureFileOutput *)output didPauseRecordingToOutputFileAtURL:(NSURL *)fileURL fromConnections:(NSArray<AVCaptureConnection *> *)connections NS_AVAILABLE_MAC(10_7){
+    
+     NSLog(@"暂停写入文件");
+}
+
+- (void)captureOutput:(AVCaptureFileOutput *)output didResumeRecordingToOutputFileAtURL:(NSURL *)fileURL fromConnections:(NSArray<AVCaptureConnection *> *)connections NS_AVAILABLE_MAC(10_7){
+      NSLog(@"恢复 写入文件");
+    
+}
+
+- (void)captureOutput:(AVCaptureFileOutput *)output didFinishRecordingToOutputFileAtURL:(NSURL *)outputFileURL fromConnections:(NSArray<AVCaptureConnection *> *)connections error:(nullable NSError *)error{
+    
+      NSLog(@"结束写入文件");
 }
 
 @end
